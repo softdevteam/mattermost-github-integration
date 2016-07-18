d664d122f694a639f1f8e8d69ade0c90ef1f32ed
Lukas Diekmann <lukas.diekmann@gmail.com>
2016-07-18 15:05:36 +0100

    Deactivate debug mode by default

--- server.py
+++ server.py
@@ -119,4 +119,4 @@ def get_hook_info(data):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
