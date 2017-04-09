88cf3aa8d1090f5744370e15d93739747a23f823
Travis Rowland <travis.rowland@gmail.com>
2017-04-09 05:54:55 -0700

    Getting initial dev environment setup

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-import os
+import config
+import hashlib
+import hmac
+import json
+
 import requests
 from flask import Flask
 from flask import request
-import json
-import config
-import hmac
-import hashlib
 
 from payload import PullRequest, PullRequestComment, Issue, IssueComment, Repository, Branch, Push, Tag, CommitComment
 
@@ -13,11 +13,12 @@ app = Flask(__name__)
 
 SECRET = hmac.new(config.SECRET, digestmod=hashlib.sha1) if config.SECRET else None
 
+
 @app.route(config.SERVER['hook'] or "/", methods=['POST'])
 def root():
     if request.json is None:
-       print 'Invalid Content-Type'
-       return 'Content-Type must be application/json and the request body must contain valid JSON', 400
+        print 'Invalid Content-Type'
+        return 'Content-Type must be application/json and the request body must contain valid JSON', 400
 
     if SECRET:
         signature = request.headers.get('X-Hub-Signature', None)
@@ -85,6 +86,7 @@ def root():
     else:
         return "Not implemented", 400
 
+
 def post(text, url, channel):
     data = {}
     data['text'] = text
@@ -96,7 +98,9 @@ def post(text, url, channel):
     r = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
 
     if r.status_code is not requests.codes.ok:
-        print 'Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (url, r.status_code, r.json())
+        print 'Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (
+        url, r.status_code, r.json())
+
 
 def get_hook_info(data):
     if 'repository' in data:
@@ -122,6 +126,6 @@ def get_hook_info(data):
 if __name__ == "__main__":
     app.run(
         host=config.SERVER['address'] or "0.0.0.0"
-    ,   port=config.SERVER['port'] or 5000
-    ,   debug=False
+        , port=config.SERVER['port'] or 5000
+        , debug=False
     )
