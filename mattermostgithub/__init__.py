eb8287483d31c4af2b3a7fe81619fb59490a6f73
David Cormier <david.cormier@erudit.org>
2017-05-17 14:38:02 -0400

    read config from MGI_CONFIG_FILE env variable

--- mattermostgithub/__init__.py
+++ mattermostgithub/__init__.py
@@ -1,4 +1,21 @@
+import os
+import sys
 from flask import Flask
 app = Flask(__name__)
+
+if os.environ.get('MGI_CONFIG_FILE'):
+    module_name = "mattermostgithub.config"
+    file_path = os.environ.get('MGI_CONFIG_FILE')
+    try:
+        # python 3
+        import importlib.util
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        sys.modules[module_name] = module
+    except:
+        # python 2
+        import imp
+        config = imp.load_source('mattermostgithub.config', file_path)
 
 import mattermostgithub.server
