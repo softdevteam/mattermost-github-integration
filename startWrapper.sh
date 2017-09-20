91f7e63c9ce29fb822f7ad152c637c94d99ee9f3
linxcat <clavoy_comp@hotmail.com>
2017-09-20 10:50:17 -0400

    env agnostic deploy

--- startWrapper.sh
+++ startWrapper.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-sed -e 's/octoproxysecretvault/${OCTOSECRET}/g' config.py.template > config.py
+sed -e "s/octoproxysecretvault/${OCTOSECRET}/g" config.py.template > config.py
+sed -i -e "s/replacemmgihost/${MATTERMOST_HOST}/g" condig.py
 ${MMGIPYTHON} ${MMGIPATH}server.py
