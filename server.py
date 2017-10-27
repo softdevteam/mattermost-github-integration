874d3a1893d72f5e4db8dcdec8dde4859ce15429
Lukas Diekmann <lukas.diekmann@gmail.com>
2017-10-27 11:32:47 +0100

    Fixed legacy method to run this which was broken by latest refactoring

--- server.py
+++ server.py
@@ -1,2 +1,10 @@
 from mattermostgithub import app
-app.run()
+try:
+    import config
+except ImportError:
+    from mattermostgithub import config
+app.run(
+    host=config.SERVER['address'] or "0.0.0.0",
+    port=config.SERVER['port'] or 5000,
+    debug=False
+)
